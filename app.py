ead7bb4cb7b2717edc87aa5c8992f5b64913e254
kwon4960 <kwon4960@naver.com>
2020-06-15 23:21:14 +0900

    게임 추가

--- app.py
+++ app.py
@@ -1,124 +1,42 @@
-import random
+from flask import Flask, request, render_template, redirect, url_for, abort
 
-def game(num, character ):
+import game
+import json
 
-    if num == 1:
-
-        print("1. 공격 2. 아이템 사용.")
-
-        num = int(input("선택: "))
-
-        if num ==1:
-
-             print(random.choice(character["skill"]))
-
-             print("강도 제압후 경찰에 넘기다.")
-
-        elif num == 2:
-
-            print(random.choice(character["items"]))
-
-            print("강도 기절후 경찰에 넘기다.")
-
-    elif num == 2:  
-
-        print("도망가다 강도에게 맞아서 HP 0")
-
-    else:
-
-        print("입력 잘못 해서 게임 끝.")
-
- 
-
-def charact(name):
-
-    character = {
-
-        "name": name,
-
-        "level": 1,
-
-        "hp": 100,
-
-        "items": ["전기충격기", "야구방망이", "휴대폰"],
-
-        "skill": ["이단돌려차기", "니킥", "뒤돌려차기"]
-
-    }
-
-    print("{0}님 반갑습니다. (HP {1})으로 게임을 시작 합니다.".format(character["name"], character["hp"]))
-
-    return character
-
- 
-
-def save_game(filename, charact):
-
-    f = open(filename, "w", encoding="utf-8")
-
-    for key in character:
-
-        print("%s:%s" % (key, character[key]))
-
-        f.write("%s:%s\n" % (key, character[key]))
-
-    f.close()
-
- 
-
-name = input("이름을 입력하시오: ")
-
- 
-
-character = charact(name)
-
- 
-
- 
-
-save_game("save.txt", character)
-
- 
-
- 
-
-print("길을 가다가 강도를 만났습니다.")
-
-while(True):
-
-    try:
-
-        print("1. 싸운다 2. 도망간다")
-
-        num = int(input("선택: "))
-
-        break
-
-    except:
-
-        print("숫자만 입력.")
-
-game(num, character)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from flask import Flask, request, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '메인페이지'
+
+@app.route('/hello/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/hello/<name>')
+def hellovar(name):
+    character = game.set_charact(name)
+    return render_template('gamestart.html', data=character)
+    
+@app.route('/gamestart')
+def gamestart():
+    with open("static/save.txt", "r", encoding='utf-8') as f:
+        data = f.read()
+        character = json.loads(data)
+        print(character['items'])
+    return  "{} 이 {} 아이템을 사용 해서 이겼다".format(character["name"], character["items"][0])
+
+@app.route('/input/<int:num>')
+def input_num(num):
+    if num == 1:
+        return "도라에몽"
+    elif num == 2:
+        return "진구"
+    elif num == 3:
+        return "퉁퉁이"
+    else:
+        return "없어요"
+        
 @app.route('/')
 def hello():
     return '아이디 또는 패스워드를 확인 하세요.' 
@@ -128,8 +46,10 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        id = request.form['id'] 
+        id = request.form['id']
         pw = request.form['pw']
+        print (id,type(id))
+        print (pw,type(pw))
         if id == 'abc' and pw == '1234':
             return "안녕하세요~ {} 님".format(id)
         else:
@@ -147,10 +67,16 @@ def method():
         num = request.form['num']
         name = request.form['name']
         print(num, name)
+        with open("static/save.txt"."w", encoding='utf-8') as f:
+            f.write("%s,%s" % (num, name))
         return 'POST 이다. 학번은: {} 이름은: {}'.format(num, name)  
-         
 
-
+@app.route('/getinfo')
+def getinfo():
+    with open("static/save.txt", "r" encoding='utf-8') as file:
+        student = file.read().split(',')
+    return '번호 : {}, 이름 : {}'.format(student[0], student[1])
+    
 @app.route('/naver')
 def naver():
     return redirect("https://www.naver.com/")
@@ -169,6 +95,10 @@ def move_site(site):
         return redirect(url_for('naver'))
     elif site == 'daum':
         return redirect(url_for('daum'))
+
+@app.route('/img')
+def img():
+    return render_template("image.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
