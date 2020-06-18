80e6af04e57182e5b777302c0b4c6c3a34f87a4a
kwon4960 <kwon4960@naver.com>
2020-06-18 22:06:44 +0900

    db 소스 적용

--- app.py
+++ app.py
@@ -68,15 +68,15 @@ def method():
         num = request.form['num']
         name = request.form['name']
         print(num, name)
-        with open("static/save.txt"."w", encoding='utf-8') as f:
-            f.write("%s,%s" % (num, name))
+        dbdb.insert_data(num, name)
         return 'POST 이다. 학번은: {} 이름은: {}'.format(num, name)  
 
 @app.route('/getinfo')
 def getinfo():
-    with open("static/save.txt", "r" encoding='utf-8') as file:
-        student = file.read().split(',')
-    return '번호 : {}, 이름 : {}'.format(student[0], student[1])
+    ret = dbdb.select_all()
+    print(ret[3])
+    return render_template('getinfo.html', data=ret)
+    # return '번호 : {}, 이름 : {}'.format(student[0], student[1])
     
 @app.route('/naver')
 def naver():
@@ -103,5 +103,4 @@ def img():
 
 if __name__ == '__main__':
     app.run(debug=True)
- {}{}{}{}
  
